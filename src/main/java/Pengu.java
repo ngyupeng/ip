72c1baf138ac2d2a1f5580ac644fb1b8c805f9da
ngyupeng <44664834+ngyupeng@users.noreply.github.com>
2025-08-21 15:52:20 +0800

    Refactor: tie input process to an instance of Pengu

--- src/main/java/Pengu.java
+++ src/main/java/Pengu.java
@@ -3,40 +3,7 @@ import java.util.Scanner;
 public class Pengu {
     private static final String NAME = "Pengu";
 
-    private static void greet() {
-        String greetMessage = String.format("Hello! I'm %s\n" + "What can I do for you?", NAME);
-        printMessage(greetMessage);
-    }
-
-    private static void exit() {
-        String exitMessage = "Bye. Hope to see you again soon!";
-        printMessage(exitMessage);
-    }
-
-    private static void echo(String message) {
-        printMessage(message);
-    }
-
-
-    private static void printMessage(String message) {
-        final String PADDING = " ".repeat(4);
-        printLine();
-
-        String[] lines = message.split("\n");
-        for (String line : lines) {
-            System.out.println(PADDING + line);
-        }
-
-        printLine();
-    }
-
-    private static void printLine() {
-        final String LINE = "_".repeat(60);
-        final String PADDING = " ".repeat(4);
-        System.out.println(PADDING + LINE);
-    }
-
-    public static void main(String[] args) {
+    public void run() {
         greet();
 
         Scanner scanner = new Scanner(System.in);
@@ -48,5 +15,42 @@ public class Pengu {
             }
             echo(input);
         }
+    }
+
+    private void greet() {
+        String greetMessage = String.format("Hello! I'm %s\n" + "What can I do for you?", NAME);
+        printMessage(greetMessage);
+    }
+
+    private void exit() {
+        String exitMessage = "Bye. Hope to see you again soon!";
+        printMessage(exitMessage);
+    }
+
+    private void echo(String message) {
+        printMessage(message);
+    }
+
+    private void printMessage(String message) {
+        final String PADDING = " ".repeat(4);
+        printLine();
+
+        String[] lines = message.split("\n");
+        for (String line : lines) {
+            System.out.println(PADDING + line);
+        }
+
+        printLine();
+    }
+
+    private void printLine() {
+        final String LINE = "_".repeat(60);
+        final String PADDING = " ".repeat(4);
+        System.out.println(PADDING + LINE);
+    }
+
+    public static void main(String[] args) {
+        Pengu pengu = new Pengu();
+        pengu.run();
     }
 }
