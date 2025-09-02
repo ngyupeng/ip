98e63cbc6d0b810f19e229619d97b972444ea575
ngyupeng <ngyupeng06@gmail.com>
2025-09-03 00:44:45 +0800

    Fix style

--- src/main/java/pengu/gui/Main.java
+++ src/main/java/pengu/gui/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setPengu(pengu);  // inject the Pengu instance
+            fxmlLoader.<MainWindow>getController().setPengu(pengu); // inject the Pengu instance
             fxmlLoader.<MainWindow>getController().start();
             stage.show();
         } catch (IOException e) {
